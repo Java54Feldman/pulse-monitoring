20e91874efb1eead70d970aa9c8bbaea5c5f8c27
Pavel Feldman <feldmanpk@gmail.com>
2024-11-07 23:53:47 +0200

    CW #65

--- pulse-jump-analyzer/src/main/java/telran/pulse/monitoring/App.java
+++ pulse-jump-analyzer/src/main/java/telran/pulse/monitoring/App.java
@@ -7,18 +7,40 @@ import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
 
 public class App {
+	HashMap<String, Integer> lastValues = new HashMap<>();
+
 	public void handleRequest(DynamodbEvent event, Context context) {
+		//FIXME move to DynamoDB table
 		event.getRecords().forEach(r -> {
 			Map<String, AttributeValue> map = r.getDynamodb().getNewImage();
 			if (map == null) {
 				System.out.println("No new image found");
-			} else if(r.getEventName().equals("INSERT")) {
-				System.out.printf("patientId=%s, timestamp=%s, pulseValue=%s\n",
-						map.get("patientId").getN(), map.get("timestamp").getN(),
-						map.get("value").getN());
+			} else if (r.getEventName().equals("INSERT")) {
+				String patientId = map.get("patientId").getN();
+				Integer currentValue = Integer.parseInt(map.get("value").getN());
+				String timestamp = map.get("timestamp").getN();
+				Integer lastValue = lastValues.computeIfAbsent(patientId, k -> currentValue);
+				if (isJump(currentValue, lastValue)) {
+					jumpProcessing(patientId, currentValue, lastValue, timestamp);
+				}
+				lastValues.put(patientId, lastValue);
 			} else {
 				System.out.println(r.getEventName());
 			}
 		});
+	}
+
+	private void jumpProcessing(String patientId, Integer currentValue, Integer lastValue, String timestamp) {
+		// FIXME move to DynamoDB table
+		System.out.printf("Jump: patientId: %s, lastValue is %d, currentValue is %d, timestamp: %s\n", 
+			patientId, lastValue, currentValue, timestamp);
+
+	}
+
+	private boolean isJump(Integer currentValue, Integer lastValue) {
+		//FIXME
+		float factor = 0.2f;
+		return (float) Math.abs(currentValue - lastValue) / lastValue > factor;
+		
 	}
 }
